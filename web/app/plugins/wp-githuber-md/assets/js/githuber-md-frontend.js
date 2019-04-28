f9f53d32f659e2066b3205da6dde90355ddd5ff4
ryuki <mge31027@gmail.com>
2019-04-28 01:33:26 +0000

    add plugin

---
+++ web/app/plugins/wp-githuber-md/assets/js/githuber-md-frontend.js
@@ -0,0 +1,20 @@
+(function($) {
+
+    $(function() {
+        var setting = window.md_frontend_settings;
+
+        if (setting.link_opening_method == '_blank') {
+
+            $('.post a').each(function() {
+                var link_href = $(this).attr('href');
+                if (link_href.indexOf('#') == -1) {
+                    $(this).attr('target', '_blank');
+                }
+            });
+        }
+    });
+
+ })(jQuery);
+ 
+
+
